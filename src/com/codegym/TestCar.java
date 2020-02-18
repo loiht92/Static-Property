17abf37214f3a612f1d75b1729ad86e40e96a1bf
loiminh <loiho11082015@gmail.com>
2020-02-18 21:25:01 +0700

    Add file

---
+++ src/com/codegym/TestCar.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+public class TestCar {
+    public static void main(String[] args) {
+        Car car = new Car("Toyta" , "Skyatic 3");
+        System.out.println(Car.numberOfCar);
+
+        Car car1 = new Car("Suzuki", "sdhcdjsv 3");
+        System.out.println(Car.numberOfCar);
+    }
+}
